46157f0dec687d52b0eaad3106b60352679fbc85
Mikasa Ackerman <makimabot8@gmail.com>
2026-04-17 10:11:56 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -87,7 +87,7 @@ module.exports = {
 
         if (category !== "info") {
 
-          msg += `\n╭──⭓🌺 『  ${category.toUpperCase()}  』`;
+          msg += `\n╭─⭓🌺 『  ${category.toUpperCase()}  』`;
 
 
           const names = categories[category].commands.sort();
@@ -110,7 +110,7 @@ module.exports = {
 
       const totalCommands = commands.size;
 
-      msg += `\n\n🌺╭───────────⭓\n» 𝙏𝙤𝙩𝙖𝙡 𝙘𝙢𝙙𝙨: [ ${totalCommands} ]\n`;
+      msg += `\n\n🌺╭─────────⭓\n» 𝙏𝙤𝙩𝙖𝙡 𝙘𝙢𝙙𝙨: [ ${totalCommands} ]\n`;
 
       msg += `» 𝙏𝙮𝙥𝙚 [ ${prefix}help <𝙘𝙢𝙙> ] 𝙩𝙤 𝙡𝙚𝙖𝙧𝙣 𝙪𝙨𝙖𝙜𝙚.\n ʙᴏᴛ ᴏᴡɴᴇʀ 🩶─⃝‌‌Sɪʏꫝᴍツ\n\n`;
 
