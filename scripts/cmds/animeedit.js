913f57a322c5cb77ae0d24b89c3e63361be1aef7
Mikasa Ackerman <makimabot8@gmail.com>
2026-04-17 11:19:10 +0600

    Update and rename aniedit.js to animeedit.js

--- scripts/cmds/animeedit.js
+++ scripts/cmds/animeedit.js
@@ -9,7 +9,7 @@ const baseApiUrl = async () => {
 
 module.exports = {
         config: {
-                name: "aniedit",
+                name: "animeedit",
                 aliases: ["aedit"],
                 version: "1.7",
                 author: "MahMUD",
