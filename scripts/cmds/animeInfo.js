2337406b07c0e853f394f58e4a91decf4015fc2e
Mikasa Ackerman <makimabot8@gmail.com>
2026-04-17 21:49:37 +0600

    Update animeInfo.js

--- scripts/cmds/animeInfo.js
+++ scripts/cmds/animeInfo.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 module.exports = {
   config: {
-    name: "anime",
+    name: "myanimelist",
     aliases: ["mal", "myanimelist"],
     author: "Rasin",
     countDown: 5,
