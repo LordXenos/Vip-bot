e212829834b9c847c4d5b3aa5427387172b49733
Mikasa Ackerman <makimabot8@gmail.com>
2026-04-17 22:57:21 +0600

    Update gcinfo2.js

--- scripts/cmds/gcinfo2.js
+++ scripts/cmds/gcinfo2.js
@@ -110,7 +110,7 @@ module.exports = {
       msg += `┃ 🎀 [ 𝐆𝐂 𝐍𝐚𝐦𝐞 ]: ${groupName}\n`;
       msg += `┃ 🆔 [ 𝐆𝐫𝐨𝐮𝐩 𝐈𝐃 ]: ${targetThreadID}\n`;
       msg += `┃ ✅ [ 𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 ]: ${approvalStatus}\n`;
-      msg += `┃ 😊 [ 𝐄𝐦𝐨𝐣𝐢 ]: ${groupEmoji}\n`;
+      msg += `┃ ❓ [ 𝐄𝐦𝐨𝐣𝐢 ]: ${groupEmoji}\n`;
       msg += `┃ 👥 [ 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 ]: Including ${memberCount} Members\n`;
       msg += `┃ ♂️ [ 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐚𝐥𝐞𝐬 ]: ${maleCount}\n`;
       msg += `┃ ♀️ [ 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐅𝐞𝐦𝐚𝐥𝐞𝐬 ]: ${femaleCount}\n`;
@@ -126,7 +126,7 @@ module.exports = {
 
       msg += `┃ 💬 [ 𝐓𝐨𝐭𝐚𝐥 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐞𝐬𝐬𝐚𝐠𝐞𝐬 ]: ${messageCount} msgs.\n`;
       msg += `┃ ━━━━━━━━━━━━━━━━━\n`;
-      msg += `┃ 🖤 𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡 ❤ 𝐁𝐲: Vydron\n`;
+      msg += `┃ 🖤 𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡 ✨ 𝐁𝐲: —͞SIYAM i!\n`;
       msg += `┗━━━━━━━━━━━━━━━━━┛`;
 
       return message.reply(msg);
