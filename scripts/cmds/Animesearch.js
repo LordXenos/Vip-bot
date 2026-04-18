c877db4013e4a9a2bceb376435d938931acaa14b
Mikasa Ackerman <makimabot8@gmail.com>
2026-04-18 20:54:03 +0600

    Update Animesearch.js

--- scripts/cmds/Animesearch.js
+++ scripts/cmds/Animesearch.js
@@ -30,7 +30,7 @@ module.exports = {
     longDescription: {
       en: "search for anime edits video",
     },
-    category: "fun",
+    category: "anime",
     guide: {
       en: "{p}{n} [query]",
     },
